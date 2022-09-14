bc2859ed6912ad101eabcbb3f362fcdadde47bd4
ZoranDEV <zoranpavicevic098@gmail.com>
2022-09-14 13:10:13 +0200

    start on faq and in navbar crate links to pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import CovidWarning from "./components/CovidWarning";
 import Home from "./components/Home";
 import Covid from "./components/Covid";
+import Faq from "./components/Faq";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/covid-info" element={<Covid />} />
+        <Route path="/faq" element={<Faq />} />
       </Routes>
     </Router>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,13 +63,16 @@ const Navbar = () => {
             !showNavbar && "hidden"
           } w-full bg-rose-500 flex flex-col items-center justify-start overflow-hidden showNavbar absolute left-0 top-16 z-10 lg:w-[70%] lg:h-14 lg:flex-row lg:justify-end lg:bg-transparent lg:relative lg:top-0 lg:z-0`}
         >
-          {["tiquets", "Oppening hours", "About", "faq"].map((item, index) => (
-            <div
+          {["tiquets", "Oppening hours", "about", "faq"].map((item, index) => (
+            <Link
+              to={`${item === "Oppening hours" ? "/hours" : `/${item}`}`}
               key={index}
-              className="w-full h-14 p-3 text-white text-lg uppercase cursor-pointer hover:bg-rose-600 lg:w-auto lg:mr-5 lg:hover:bg-transparent lg:text-black lg:after:block lg:after:border-b-2 lg:after:border-red-500 lg:after:content-[''] lg:after:scale-x-0 lg:after:origin-left lg:after:hover:scale-x-100 lg:after:duration-500 lg:after:ease-in-out "
-            >
-              <h1 className="w-full h-full text-center">{item}</h1>
-            </div>
+              children={
+                <div className="w-full h-14 p-3 text-white text-lg uppercase cursor-pointer hover:bg-rose-600 lg:w-auto lg:mr-5 lg:hover:bg-transparent lg:text-black lg:after:block lg:after:border-b-2 lg:after:border-red-500 lg:after:content-[''] lg:after:scale-x-0 lg:after:origin-left lg:after:hover:scale-x-100 lg:after:duration-500 lg:after:ease-in-out ">
+                  <h1 className="w-full h-full text-center">{item}</h1>
+                </div>
+              }
+            />
           ))}
           {/* Log in */}
           <button className="w-full h-14 p-3 uppercase text-white text-center hover:bg-rose-600 lg:w-auto lg:h-10 lg:px-16 lg:py-2 lg:rounded lg:bg-red-600 lg:text-white lg:capitalize lg:hover:bg-red-700 lg:active:scale-95 ">
