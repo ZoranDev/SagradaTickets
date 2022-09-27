24c0212cfeed2b6a1684d9d51905dcac9b8e0075
ZoranDEV <zoranpavicevic098@gmail.com>
2022-09-27 16:46:37 +0200

    style and add transition to condition items

---
+++ src/components/ticketSteps/step2/ConditionItem.js
@@ -0,0 +1,61 @@
+// react
+import { useState } from "react";
+//icons
+import { FaExclamation } from "react-icons/fa";
+
+const info = [
+  {
+    id: "general",
+    text: "I accept the general conditions of online purchase and the terms ans conditions of the service*",
+  },
+  { id: "privacy", text: "I accept the Privacy Policy.*" },
+  {
+    id: "information",
+    text: "I agree to receive information about the Basilica, visits and activities, as well as commercial information regarding other products and related services.",
+  },
+];
+
+const ConditionItem = ({ id }) => {
+  // State show message
+  const [showReqiuredMessage, setShowRequiredMessage] = useState(false);
+
+  // mouseEnter function
+  const mouseEnter = () => setShowRequiredMessage(true);
+
+  // mouse leave function
+  const mouseLeave = () => setShowRequiredMessage(false);
+
+  return (
+    <div className="flex items-center justify-left mb-2 relative">
+      <input
+        type="checkbox"
+        id={id}
+        name={id}
+        value={id}
+        onMouseEnter={mouseEnter}
+        onMouseLeave={mouseLeave}
+        className={`w-[30px] h-[30px] mr-3 cursor-pointer bg-neutral-200 hover:bg-red-400 focus:ring-red-500 text-red-500 `}
+      />
+      {info.map((item, index) => (
+        <label key={index} htmlFor="generalConditions">
+          {item.id === id && item.text}
+        </label>
+      ))}
+
+      {/* Required message div */}
+      {(id === "general" || id === "privacy") && (
+        <div
+          className={`bg-red-500 px-3 py-1 -translate-0-9 translate-x-[40px] absolute ${
+            !showReqiuredMessage
+              ? "opacity-0"
+              : " opacity-1 transition-opacity duration-[600ms]"
+          }     text-white rounded flex items-center justify-center `}
+        >
+          Required <FaExclamation className="ml-2" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ConditionItem;
--- src/components/ticketSteps/step2/TicketsStep2.js
+++ src/components/ticketSteps/step2/TicketsStep2.js
@@ -1,5 +1,6 @@
 // components
 import Step2Item from "./Step2Item";
+import ConditionItem from "./ConditionItem";
 
 const TicketsStep2 = () => {
   return (
@@ -9,22 +10,7 @@ const TicketsStep2 = () => {
       {/* Accept conditions */}
       <div>
         {["general", "privacy", "information"].map((item, index) => (
-          <div key={index} className="flex items-center justify-left mb-2 ">
-            <input
-              type="checkbox"
-              id={item}
-              name={item}
-              value={item}
-              className={`w-[30px] h-[30px] mr-3 cursor-pointer appearance-none indeterminate:bg-gray-300`}
-            />
-            <label htmlFor="generalConditions">
-              {item === "general" &&
-                "I accept the general conditions of online purchase and the terms ans conditions of the service*"}
-              {item === "privacy" && "I accept the Privacy Policy.*"}
-              {item === "information" &&
-                "I agree to receive information about the Basilica, visits and activities, as well as commercial information regarding other products and related services."}
-            </label>
-          </div>
+          <ConditionItem key={index} id={item} />
         ))}
         <p>(*) Required</p>
       </div>
