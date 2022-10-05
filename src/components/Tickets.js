6d462d56f45caf8963ecc4fed1b30dbe67ed855c
ZoranDEV <zoranpavicevic098@gmail.com>
2022-10-05 08:56:20 +0200

    macke tickets navbar responsive

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -21,68 +21,70 @@ const Tickets = () => {
   };
 
   return (
-    <div className="w-full py-10 px-2 flex flex-col items-center justify-evenly lg:flex-col lg:items-start">
-      {/* New design */}
-      <div className="w-full mx-auto px-[100px] py-5 bg-transparent border-2 border-black ">
-        {/* Active step section */}
-        <div className={`w-full flex items-center justify-center mb-5`}>
-          {[
-            { step: 1, text: "Select your tickets" },
-            {
-              step: 2,
-              text: "Personal details",
-            },
-            {
-              step: 3,
-              text: "Payment",
-            },
-            {
-              step: 4,
-              text: "Confirmation",
-            },
-          ].map((obj, index) => (
-            <div key={index} className="relative w-[200px]">
-              <div
-                className={`relative -left-3 w-[50px] h-[50px] ${
-                  obj.step === activeStep ? "bg-black" : "bg-neutral-400"
-                }  text-white text-xl rounded-full border-[1px] border-white flex items-center justify-center z-10`}
-              >
-                {obj.step === 1 ? (
-                  <FaTicketAlt />
-                ) : obj.step === 2 ? (
-                  <FaUserAlt />
-                ) : obj.step === 3 ? (
-                  <FaCreditCard />
-                ) : (
-                  <FaCheck />
-                )}
-              </div>
-
-              <div
-                className={`absolute top-2/4 -translate-y-2/4 w-full pl-11 py-1 text-white rounded-md ${
-                  obj.step === activeStep ? "bg-black" : "bg-neutral-400"
-                }`}
-              >
-                {obj.text}
-              </div>
+    <div className="w-full mx-auto px-3 py-5 bg-transparent lg:px-[100px]">
+      {/* Display active step section */}
+      <div className={`w-full flex items-center justify-center mb-5`}>
+        {[
+          { step: 1, text: "Select your tickets" },
+          {
+            step: 2,
+            text: "Personal details",
+          },
+          {
+            step: 3,
+            text: "Payment",
+          },
+          {
+            step: 4,
+            text: "Confirmation",
+          },
+        ].map((obj, index) => (
+          <div
+            key={index}
+            className="relative w-[200px] flex justify-center lg:justify-start"
+          >
+            <div
+              className={`relative -left-3 w-[50px] h-[50px] ${
+                obj.step === activeStep ? "bg-black" : "bg-neutral-400"
+              }  text-white text-xl rounded-full border-[1px] border-white flex items-center justify-center z-20`}
+            >
+              {obj.step === 1 ? (
+                <FaTicketAlt />
+              ) : obj.step === 2 ? (
+                <FaUserAlt />
+              ) : obj.step === 3 ? (
+                <FaCreditCard />
+              ) : (
+                <FaCheck />
+              )}
             </div>
-          ))}
-        </div>
 
-        {/* Steps */}
-        <TicketProvider>
-          {activeStep === 1 && <TicketsStep1 />}
-          {activeStep === 2 && <TicketsStep2 />}
-          {activeStep === 3 && <TicketsStep3 />}
-          {activeStep === 4 && <TicketsStep4 />}
-          {activeStep !== 4 && (
-            <CalculatePrice
-              changeActiveStep={changeActiveStep}
-              activeStep={activeStep}
-            />
-          )}
-        </TicketProvider>
+            <div
+              className={`${
+                obj.step === activeStep ? "bg-black" : "bg-neutral-400"
+              } ${
+                obj.step === 4 && "hidden lg:block"
+              } w-full absolute top-2/4 -translate-y-2/4 translate-x-2/4 z-10 py-1 text-white rounded-md lg:translate-x-0 lg:pl-11`}
+            >
+              <h1 className="text-[0px] lg:text-lg">{obj.text}</h1>
+            </div>
+          </div>
+        ))}
       </div>
+
+      {/* Steps */}
+      <TicketProvider>
+        {activeStep === 1 && <TicketsStep1 />}
+        {activeStep === 2 && <TicketsStep2 />}
+        {activeStep === 3 && <TicketsStep3 />}
+        {activeStep === 4 && <TicketsStep4 />}
+        {activeStep !== 4 && (
+          <CalculatePrice
+            changeActiveStep={changeActiveStep}
+            activeStep={activeStep}
+          />
+        )}
+      </TicketProvider>
     </div>
   );
 };
